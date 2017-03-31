b05ede84c38fae5278f69e88f2cd9dc4e27d854d
digiou <digitalbckp@gmail.com>
2017-03-31 11:01:08 +0300

    Streamline env loading

--- load_environment.r
+++ load_environment.r
@@ -1,8 +1,15 @@
 import(twitteR)
 import(RMySQL)
 
+setwd("~/git/twitter-senti-bio")
+readRenviron(paste(getwd(), "/.Renviron"))
+
 api_key <- Sys.getenv("api_key")
 api_secret <- Sys.getenv("api_secret")
 
 access_token <- Sys.getenv("access_token")
 access_secret <- Sys.getenv("access_secret")
+
+db_user <- Sys.getenv("db_user")
+db_pass <- Sys.getenv("db_pass")
+db_name <- Sys.getenv("db_name")
