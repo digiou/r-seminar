eff605fb4aee4fc41777d83565f6206c78014e86
digiou <digitalbckp@gmail.com>
2017-03-31 10:42:49 +0300

    Add Renviron and script to load credentials

---
+++ load_environment.r
@@ -0,0 +1,8 @@
+import(twitteR)
+import(RMySQL)
+
+api_key <- Sys.getenv("api_key")
+api_secret <- Sys.getenv("api_secret")
+
+access_token <- Sys.getenv("access_token")
+access_secret <- Sys.getenv("access_secret")
