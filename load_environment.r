602da78329e70e89bcd72b285b5ceef023d7231b
digiou <digitalbckp@gmail.com>
2017-04-04 11:38:13 +0300

    Add list of scientific account names and two lines of example code from timeline

--- load_environment.r
+++ load_environment.r
@@ -1,8 +1,8 @@
-import(twitteR)
-import(RMySQL)
+library(twitteR)
+library(RMySQL)
 
 setwd("~/git/twitter-senti-bio")
-readRenviron(paste(getwd(), "/.Renviron"))
+readRenviron(paste(getwd(), "/.Renviron", sep=""))
 
 api_key <- Sys.getenv("api_key")
 api_secret <- Sys.getenv("api_secret")
@@ -13,3 +13,11 @@ access_secret <- Sys.getenv("access_secret")
 db_user <- Sys.getenv("db_user")
 db_pass <- Sys.getenv("db_pass")
 db_name <- Sys.getenv("db_name")
+
+scientific_ids_table <- table(factor(c("754937215302631424")))
+
+setup_twitter_oauth(api_key, api_secret, access_token, access_secret)
+
+register_mysql_backend("biotweets", "127.0.0.1", "root", "topkek")
+
+tweets <- userTimeline("754937215302631424", 5, excludeReplies = TRUE)
