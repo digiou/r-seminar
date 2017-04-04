7e772e12cd72d1723854d5113f42255182ba667a
Dimitris Giouroukis <dgiouroukis@customedialabs.com>
2017-04-04 19:54:44 +0300

    Add more twitter IDs for journals and streamline low return count from search api

--- load_environment.r
+++ load_environment.r
@@ -15,39 +15,56 @@ db_user <- Sys.getenv("db_user")
 db_pass <- Sys.getenv("db_pass")
 db_name <- Sys.getenv("db_name")
 
-scientific_ids_table <- array(c("754937215302631424"))
+scientific_ids_table <- array(c("754937215302631424", 
+                                "20769532",
+                                "52889950",
+                                "487833518",
+                                "1737474631",
+                                "806253116182188032",
+                                "87706747",
+                                "523433085",
+                                "224991073",
+                                "1608950586",
+                                "403163083",
+                                "48473838",
+                                "273984889",
+                                "18049298",
+                                "3121167195"))
+
+orgz_ids_table <- array(c("15862891"))
 
 setup_twitter_oauth(api_key, api_secret, access_token, access_secret)
 
 register_mysql_backend("biotweets", "127.0.0.1", "root", "topkek")
 
-tweets <- userTimeline("754937215302631424", 5, excludeReplies = TRUE)
-
-tweetsDF <- twListToDF(tweets)
-
-orderedIds <- mixedorder(tweetsDF$id)
-
-minId <- tweetsDF$id[orderedIds[1]]
-
-olderTweets <- userTimeline("754937215302631424", 5, excludeReplies = TRUE, maxID = as.numeric(minId) - 1)
-
 gatherAllTweets <- function(accountId) { 
-       res = list()
-       tweets <- userTimeline(accountId, 1, excludeReplies = TRUE)
-       res <- list(res, tweets)
-       while(length(tweets) != 0) {
-           tweetsDF <- twListToDF(tweets)
-           orderedIds <- mixedorder(tweetsDF$id)
-           minId <- tweetsDF$id[orderedIds[1]]
-           tweets <- userTimeline(accountId, 3200, excludeReplies = TRUE, maxID = as.numeric(minId) - 1)
-           if(length(tweets) == 0) {
-             print("GOT ZERO")
-           } else {
-             res <- list(res, tweets)
-             print(paste("GOT ", length(tweets), " elements!"))
-           }
-     }
-     return(res)
+    res = list()
+    tweets <- userTimeline(accountId, 3200, excludeReplies = TRUE)
+    res <- list(res, tweets)
+    print(paste("GOT ", length(tweets), " elements!"))
+    message("Sleeping for 5 minutes, got initial request!")
+    Sys.sleep(300)
+    while(length(tweets) != 0) {
+       tweetsDF <- twListToDF(tweets)
+       orderedIds <- mixedorder(tweetsDF$id)
+       minId <- tweetsDF$id[orderedIds[1]]
+       tweets <- userTimeline(accountId, 3200, excludeReplies = TRUE, maxID = as.numeric(minId) - 1)
+       if(length(tweets) == 0) {
+         print("GOT ZERO")
+       } else if(length(tweets) < 100 && length(tweets) >= 1){
+         res <- list(res, tweets)
+         print(paste("GOT ", length(tweets), " elements!"))
+         message("got low count of tweets!")
+         tweets = list()
+       } else {
+         res <- list(res, tweets)
+         print(paste("GOT ", length(tweets), " elements!"))
+         message("Sleeping for 5 minutes, got a request!")
+         Sys.sleep(300)
+       }
+    }
+    message("Finished while loop")
+    return(res)
 }
 
 gatherAllTweetsWithTry <- function(accountId) {
@@ -58,6 +75,12 @@ gatherAllTweetsWithTry <- function(accountId) {
     message("Hit a warning, sleeping for 15 minutes")
     Sys.sleep(630)
     gatherAllTweetsWithTry(accountId)
+  },
+  error=function(cond) {
+    message("Hit an error, sleeping for 15 minutes")
+    Sys.sleep(630)
+    gatherAllTweetsWithTry(accountId)
   }
   )
+  return(out)
 }
