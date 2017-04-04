8715ce397d4cb0a905106cbd48da9353cf2bb5e7
digiou <digitalbckp@gmail.com>
2017-04-05 00:09:12 +0300

    Add list of eco org IDs, multiple mysql backends

--- store_recent_tweets.R
+++ store_recent_tweets.R
@@ -29,42 +29,54 @@ scientific_ids_table <- array(c("754937215302631424",
                                 "48473838",
                                 "273984889",
                                 "18049298",
-                                "3121167195"))
+                                "3121167195",
+                                "2782211491"))
 
-orgz_ids_table <- array(c("15862891"))
+orgz_ids_table <- array(c("15862891",
+                          "13201312",
+                          "9890492",
+                          "3459051",
+                          "493889332",
+                          "859444603",
+                          "17471979",
+                          "1652460752",
+                          "29221344",
+                          "3044804453",
+                          "2292423140",
+                          ""))
 
 setup_twitter_oauth(api_key, api_secret, access_token, access_secret)
 
-register_mysql_backend("biotweets", "127.0.0.1", "root", "topkek")
+register_mysql_backend("scitweets", "127.0.0.1", "root", "topkek")
 
 gatherAllTweets <- function(accountId) { 
-    res = list()
-    tweets <- userTimeline(accountId, 3200, excludeReplies = TRUE)
-    res <- c(res, tweets)
-    message(paste("GOT ", length(tweets), " elements!"))
-    message("Sleeping for 5 minutes, got initial request!")
-    Sys.sleep(300)
-    while(length(tweets) != 0) {
-       tweetsDF <- twListToDF(tweets)
-       orderedIds <- mixedorder(tweetsDF$id)
-       minId <- tweetsDF$id[orderedIds[1]]
-       tweets <- userTimeline(accountId, 3200, excludeReplies = TRUE, maxID = as.numeric(minId) - 1)
-       if(length(tweets) == 0) {
-         print("GOT ZERO")
-       } else if(length(tweets) < 20 && length(tweets) >= 1){
-         res <- c(res, tweets)
-         message(paste("GOT ", length(tweets), " elements!"))
-         message("got low count of tweets!")
-         tweets = list()
-       } else {
-         res <- c(res, tweets)
-         message(paste("GOT ", length(tweets), " elements!"))
-         message("Sleeping for 5 minutes, got a request!")
-         Sys.sleep(300)
-       }
+  res = list()
+  tweets <- userTimeline(accountId, 3200, excludeReplies = TRUE)
+  res <- c(res, tweets)
+  message(paste("GOT ", length(tweets), " elements!"))
+  message("Sleeping for 5 minutes, got initial request!")
+  Sys.sleep(300)
+  while(length(tweets) != 0) {
+    tweetsDF <- twListToDF(tweets)
+    orderedIds <- mixedorder(tweetsDF$id)
+    minId <- tweetsDF$id[orderedIds[1]]
+    tweets <- userTimeline(accountId, 3200, excludeReplies = TRUE, maxID = as.numeric(minId) - 1)
+    if(length(tweets) == 0) {
+      print("GOT ZERO")
+    } else if(length(tweets) < 20 && length(tweets) >= 1){
+      res <- c(res, tweets)
+      message(paste("GOT ", length(tweets), " elements!"))
+      message("got low count of tweets!")
+      tweets = list()
+    } else {
+      res <- c(res, tweets)
+      message(paste("GOT ", length(tweets), " elements!"))
+      message("Sleeping for 5 minutes, got a request!")
+      Sys.sleep(300)
     }
-    message("Finished while loop")
-    return(res)
+  }
+  message("Finished while loop")
+  return(res)
 }
 
 gatherAllTweetsWithTry <- function(accountId) {
@@ -86,8 +98,17 @@ gatherAllTweetsWithTry <- function(accountId) {
 }
 
 for (account_id in scientific_ids_table) {
-  message(paste("Getting tweets for user: ", account_id))
+  message(paste("Getting tweets for user: ", account_id, " in scientific publishers"))
   tweets <- gatherAllTweetsWithTry(account_id)
   store_tweets_db(tweets)
-  message("Done")
+  message(paste("Done with: ", account_id))
+}
+
+register_mysql_backend("orgtweets", "127.0.0.1", "root", "topkek")
+
+for(account_id in orgz_ids_table) {
+  message(paste("Getting tweets for user: ", account_id, " in public organizations"))
+  tweets <- gatherAllTweetsWithTry(account_id)
+  store_tweets_db(tweets)
+  message(paste("Done with: ", account_id))
 }
