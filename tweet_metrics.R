c5eae02deca3bb7b5aeaa3887552744467609b9d
Dimitrios Giouroukis <digitalbckp@gmail.com>
2017-04-07 02:39:32 +0300

    Add corpus cleaning procedures

--- tweet_metrics.R
+++ tweet_metrics.R
@@ -5,4 +5,28 @@ library(ggplot2)
 
 source("load_environment.R")
 
-sci_tweets <- load_tweets_db(table_name = sci_table_name)
+register_mysql_backend(db_name, db_host, db_user, db_pass)
+
+sci_tweets <- load_tweets_db(as.data.frame = TRUE, table_name = sci_table_name)
+
+#org_tweets <- load_tweets_db(as.data.frame = TRUE, table_name = org_table_name)
+
+sci_corpus <- Corpus(VectorSource(sci_tweets$text))
+
+#org_corpus <- Corpus(VectorSource(org_tweets$text))
+
+cleanDatum <- function(document) {
+  clean_doc <- stripWhitespace(document)
+  clean_doc <- tolower(clean_doc)
+  clean_doc <- removeWords(clean_doc, stopwords(kind = "en"))
+  clean_doc <- removePunctuation(clean_doc)
+  clean_doc <- removeNumbers(clean_doc)
+  return(clean_doc)
+}
+
+cleanCorpus <- function(corpus) {
+  clean_corpus <- tm_map(corpus, cleanDatum)
+  return(clean_corpus)
+}
+
+sci_corpus <- cleanCorpus(sci_corpus)
