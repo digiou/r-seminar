6c8212e150ee108f783ec7b773a02c957e699c48
Dimitris Giouroukis <dgiouroukis@customedialabs.com>
2017-04-06 14:39:20 +0300

    Fix new variable name loading in .Renviron

--- load_environment.R
+++ load_environment.R
@@ -8,4 +8,5 @@ access_secret <- Sys.getenv("access_secret")
 
 db_user <- Sys.getenv("db_user")
 db_pass <- Sys.getenv("db_pass")
-db_name <- Sys.getenv("db_name")
+sci_db_name <- Sys.getenv("sci_db_name")
+org_db_name <- Sys.getenv("org_db_name")
